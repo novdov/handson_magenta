84b4ca9edf8fce4f1e0f1d3f94fb228efc12cd0f
novdov <rpwjd56@gmail.com>
2020-04-20 14:10:27 +0900

    vae) Apply formatting

--- vae/sampling.py
+++ vae/sampling.py
@@ -10,7 +10,7 @@ def sample(
     model_name: str,
     num_steps_per_sample: int,
     output_dir: Union[str, Path],
-    **sample_kwargs
+    **sample_kwargs,
 ) -> List[NoteSequence]:
     model = utils.get_model(model_name)
     sample_sequences = model.sample(n=2, length=num_steps_per_sample, **sample_kwargs)
